d47db79b690ec06eac56e405092876a95988887a
tatyderb <tatyderb@gmail.com>
2025-05-16 10:34:19 +0300

    Интеграция REST API и разбора урока в виде одного файла урока в формате markdown

--- h1_h2_steps/h1_h2_steps.py
+++ h1_h2_steps/h1_h2_steps.py
@@ -27,16 +27,32 @@ parse_h2 = pp.AtLineStart(h2_start) + pp.Optional(skip) + pp.Optional(task_type,
 
 
 def parse_text(text):
+    """
+    Разбирает текс в формате markdown и возвращает словарь
+    {
+        'lesson_id': id урока,
+        'lang': язык задач на программирование (опционально),
+        'title': заголовок урока (только для человека, нигде не используется)
+        'steps': [{
+            'type': тип шага,
+            'skip': пропускать шаг при апдейте или нет,
+            'header': текст в теге <h2>,
+            'text': текст шага в формате markdown одной строкой
+       }]
+    }
+    """
+
     lines = [line for line in text.splitlines()]
     results = []
     
     current_h2 = None
     current_text = []
+    lesson_data = {'steps': []}
 
     for line in lines:
         if parse_h1.matches(line):
             lesson_title_result = parse_h1.parseString(line)
-            results.append({'lesson_title': lesson_title_result.lesson_title})
+            lesson_data['lesson_title'] = lesson_title_result.lesson_title
 
             continue
 
@@ -46,41 +62,52 @@ def parse_text(text):
 
         if parse_lesson_id.matches(line):
             lesson_id_result = parse_lesson_id.parseString(line)
-            results.append({'lesson_id': lesson_id_result.lesson_id})
+            lesson_data['lesson_id'] = lesson_id_result.lesson_id
             
             continue
 
-        elif len(results) < 1:
-            continue
+        # elif len(results) < 1:
+        #     continue
 
 
         if parse_lang.matches(line):
             lang_result = parse_lang.parseString(line)
-            results.append({'lang': lang_result.lang})
+            lesson_data['lang'] = lang_result.lang
             
             continue
 
-        elif len(results) < 2:
-            continue
+        # elif len(results) < 2:
+        #     continue
 
 
         if parse_h2.matches(line):
-            h2_current = parse_h2.parseString(line)
             if current_h2:
-                results.append({'h2': current_h2, 'text': "\n".join(current_text)})
+                current_h2['text'] = "\n".join(current_text)
+                lesson_data['steps'].append(current_h2)
+                # print('---------------------------------------------------------')
+                # print(f'{current_text=}')
+                # print('---------------------------------------------------------')
                 current_text = []
 
+            h2_current = parse_h2.parseString(line)
             current_h2 = {'type': h2_current.type,
                           'skip': 'True' if h2_current.skip else 'False', 
-                          'header': h2_current.header}
+                          'header': h2_current.header,
+                          'text': ''
+                          }
+            # print(f'{current_h2=}')
         elif current_h2:
             current_text.append(line)
+            # print(f'{current_text=}')
 
     if current_h2:
-        results.append({'h2': current_h2, 'text': "\n".join(current_text)})
+        current_h2['text'] = "\n".join(current_text)
+        lesson_data['steps'].append(current_h2)
+        # print('---------------------------------------------------------')
+        # print(f'{current_text=}')
+        # print('---------------------------------------------------------')
 
+    # print('===========================================================')
+    print(lesson_data)
 
-    for result in results:
-        print(result, '\n')
-
-    return results
+    return lesson_data
